5593292283965f43a61d64124c005d498c115195
JOPAPIZDAZALUPA <mihailmuzykow@gmail.com>
2026-04-09 05:56:48 +0100

    made expression class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 #include <string>
 
+class Expression{
+    private:
+        char variables[3];
+        int results[8];
+
+    public:
+        void setVariables(char input[3]){
+            for (int i = 0; i < 3; i++){
+                variables[i] = input[i];
+            }
+        }
+
+        void setResults(int input[8]){
+            for (int i = 0; i < 8; i++){
+                results[i] = input[i];
+            }
+        }
+
+        int combinations[8][3] = {
+            {0,0,0},
+            {0,0,1},
+            {0,1,0},
+            {0,1,1},
+            {1,0,0},
+            {1,0,1},
+            {1,1,0},
+            {1,1,1}
+        };
+};
+
 class Operator{
 protected:
     char variables[2];
